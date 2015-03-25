f1e3ccc57a3221254236fee8aea877ad88e5a458
Michael Kennedy <mikeckennedy@gmail.com>
2015-03-25 07:24:22 -0700

    Demos for session 1 and 2

---
+++ mongdb_intro/DevWeekMongoDBStarter/DevWeekMongoDBStarter/Data/MongoDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DevWeekMongoDBStarter.Data
+{
+	public class MongoDbContext : MongoDB.Kennedy.MongoDbDataContext
+	{
+		public MongoDbContext() : base("DevWeekDemo_2015")
+		{
+		}
+
+		public IQueryable<Book> Books
+		{
+			get { return base.GetCollection<Book>(); }
+		}
+	}
+
+	//[BsonIgnoreExtraElements]
+	public class Book
+	{
+		public ObjectId Id { get; set; }
+		public string Title { get; set; }
+		//public List<Author> Authors { get; set; }
+		//public List<Author> PrimaryAuthors
+		//{
+		//	get; set; }
+		public int PageCount { get; set; }
+		public DateTime PublishedDate { get; set; }
+
+		[BsonExtraElements]
+		public BsonDocument AdditionalData { get; set; }
+
+		public Book()
+		{
+			//this.PrimaryAuthors = new List<Author>();
+		}
+	}
+
+	public class Author
+	{
+		public string Name { get; set; }
+		public byte[] Photo { get; set; }
+	}
+}
---
+++ mongdb_intro/DevWeekMongoDBStarter/DevWeekMongoDBStarter/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevWeekMongoDBStarter.Data;
+using MongoDB;
+using MongoDB.Kennedy;
+
+namespace DevWeekMongoDBStarter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//CreateSomeData();
+			FindSomeData();
+
+
+		}
+
+		private static void FindSomeData()
+		{
+			var ctx = new MongoDbContext();
+
+			var books =
+				from b in ctx.Books
+				where b.PageCount >= 200 //&& b.PrimaryAuthors.Any(a => a.Name == "Ted")
+				orderby b.PageCount descending
+				select b;
+
+			Console.WriteLine("Query is: " + books.ToMongoQuery());
+
+			foreach (var b in books)
+			{
+				Console.WriteLine("{0} with {1} pages", b.Title, b.PageCount);
+			}
+		}
+
+		private static void CreateSomeData()
+		{
+			var ctx = new MongoDbContext();
+
+
+			var book = new Book()
+			{
+				PageCount = 250,
+				Title = "Getting started with MongoDB II",
+				PublishedDate = DateTime.Now
+			};
+
+			//book.PrimaryAuthors.Add(new Author() {Name="Bill"});
+			//book.PrimaryAuthors.Add(new Author() {Name="Ted"});
+			//book.PrimaryAuthors.Add(new Author() {Name="Jeff"});
+
+			ctx.Save(book);
+		}
+	}
+}
